9e4e3bbd90f83143ba016ca598fc0c8170897499
venkatajayadeep <jayadeep101097@gmail.com>
2021-07-15 21:14:09 +0530

    Added : Die_Number

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,11 +2,12 @@ package BridgeLabz;
 
 public class SnakeAndLadder {
 
-	public static void main(String[] args){
-		int position = 0;
-		System.out.println("Welcome to Snake-Ladder Program Problem");
-		System.out.println("You are at Starting Position::" + position);
+	public static void main(String[] args)
+	{
+		int die = (int)Math.floor( ( ( Math.random() * 10 ) % 6+1));
+		System.out.println("Dice Value::"+ die);
 	}
+	
 
 	}
 
