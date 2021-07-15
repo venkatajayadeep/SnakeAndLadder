7e671f251f078fb1428c43f34460ef4ba0637fa7
venkatajayadeep <jayadeep101097@gmail.com>
2021-07-15 17:42:00 +0530

    Added : Start With Zero

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,9 +2,12 @@ package BridgeLabz;
 
 public class SnakeAndLadder {
 
-	public static void main(String[] args) {
-		System.out.println("WelCome");
+	public static void main(String[] args){
+		int position = 0;
+		System.out.println("Welcome to Snake-Ladder Program Problem");
+		System.out.println("You are at Starting Position::" + position);
+	}
 
 	}
 
-}
+
